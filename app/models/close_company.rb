c92374cdabf54b1e41cf4c5c4315ccacd1a79368
unknown <User@.(none)>
2010-10-09 21:39:19 +0200

    related companies

--- app/models/close_company.rb
+++ app/models/close_company.rb
@@ -1,2 +1,19 @@
 class CloseCompany < ActiveRecord::Base
+
+  def self.rebuild
+    connection.execute("truncate table close_companies")
+
+    Company.all.each do |company|
+      companies = FoafProxy.related_companies(name)
+      companies.each do |rel_comp|
+        ico = Company.find_ico_by_name(rel_comp)
+        unless ico.nil?
+          comp = Company.create(:name => rel_comp, :ico => ico) if (comp = Company.first?(:ico => ico)).nil?
+          CloseCompany.create(:company_id => company.id, :close_company_id => comp.id) unless C
+        end
+      end
+    end
+  end
+
+
 end
