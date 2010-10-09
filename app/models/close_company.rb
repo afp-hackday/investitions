9663c1a27b28acc7604e561c9d94f093cfbedc14
unknown <User@.(none)>
2010-10-09 21:47:11 +0200

    related companies 2 pokus

--- app/models/close_company.rb
+++ app/models/close_company.rb
@@ -1,3 +1,5 @@
+require 'lib/foaf_proxy'
+
 class CloseCompany < ActiveRecord::Base
 
   def self.rebuild
@@ -6,10 +8,10 @@ class CloseCompany < ActiveRecord::Base
     Company.all.each do |company|
       companies = FoafProxy.related_companies(name)
       companies.each do |rel_comp|
-        ico = Company.find_ico_by_name(rel_comp)
+        ico = Company.find_ico_by_name(rel_comp, nil)
         unless ico.nil?
-          comp = Company.create(:name => rel_comp, :ico => ico) if (comp = Company.first?(:ico => ico)).nil?
-          CloseCompany.create(:company_id => company.id, :close_company_id => comp.id) unless C
+          comp = Company.create(:name => rel_comp, :ico => ico) if (comp = Company.first(:conditions => ["ico = ?", ico])).nil?
+          CloseCompany.create(:company_id => company.id, :close_company_id => comp.id) 
         end
       end
     end
