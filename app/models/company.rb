58c51e0ad6fb7159adf3673536b20d904668374d
Tomas Kramar <kramar.tomas@gmail.com>
2010-10-09 17:48:57 +0200

    Add odpustene_clo advantage

--- app/models/company.rb
+++ app/models/company.rb
@@ -18,8 +18,14 @@ class Company < ActiveRecord::Base
       advantages[year][:ine_dotacie] = sum
     end
 
+    ine_dotacie = OdpusteneClo.sum(:celkova_odpustena_suma, :group => :rok, :conditions => ['ico', self[:ico]])
+    ine_dotacie.each do |year, sum|
+      advantages[year] = {} if advantages[year].nil?
+      advantages[year][:odpustene_clo] = sum
+    end
+
     advantages.each do |year, sum|
-      Advantage.create(:company_id => self[:id], :period => year, :eurofondy => sum[:eurofondy], :ine_dotacie => sum[:ine_dotacie])
+      Advantage.create(:company_id => self[:id], :period => year, :eurofondy => sum[:eurofondy], :ine_dotacie => sum[:ine_dotacie], :odpustene_clo => sum[:odpustene_clo])
     end
   end
 end
