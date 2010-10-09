2499fdef3ec3b197c11e688b2396880145d39446
unknown <User@.(none)>
2010-10-09 17:18:06 +0200

    taaaakze tuto je naladovanie nejakych veci

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,17 @@
 class Company < ActiveRecord::Base
+
+  def self.rebuild
+    Company.delete_all
+    connection.execute "INSERT INTO companies (id, ico, name)
+    select
+      null, ico_darcu,
+      (case (firma_darcu)
+               when '' then concat(meno_darcu, ' ', priezvisko_darcu) else firma_darcu
+      end) as meno
+    from
+      sponzori_stran"
+  end
+
 end
+
+
