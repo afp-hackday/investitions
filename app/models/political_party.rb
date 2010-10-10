40ca9590c3a96cbe2253bd9f221102b140d016d2
Michal Barla <barla@fiit.stuba.sk>
2010-10-10 13:22:38 +0200

    Add profits computation for political party

--- app/models/political_party.rb
+++ app/models/political_party.rb
@@ -10,5 +10,24 @@ class PoliticalParty < ActiveRecord::Base
       sponzori_stran"
   end
 
+  def profits_per_year
+    profits = Hash.new(0)
+    relevant_companies = companies
+    relevant_companies = relevant_companies + companies.collect {|company| company.close_companies}
+    relevant_companies = relevant_companies.uniq
+    relevant_companies.each do |company|
+      company.advantages.each do |advantage|
+        profits[advantage.year] += advantage.eurofondy +
+                advantage.ine_dotacie +
+                advantage.konsolidacna +
+                advantage.odpustene_clo +
+                advantage.polnodotacie +
+                advantage.privatizacia +
+                advantage.obstaravania
+      end
+    end
+    profits
+  end
+
 
 end
